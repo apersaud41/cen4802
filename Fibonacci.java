5e95774279c47bb05fdb1a9fe3c1bee296305753
apersaud41 <98132725+apersaud41@users.noreply.github.com>
2022-05-19 18:17:00 -0400

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10;
+        int y = fibonacci(n);
+        System.out.println("The " + n + "th term of the Fibonacci sequence is " + y + ".");
+    }
+
+    public static int fibonacci(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        if(n == 1) {
+            return 1;
+        }
+        
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
